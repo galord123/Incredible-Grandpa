a0a3c5a9a7940eadee39356c9b1acf90f308623c
gal holtzman <galbuba@gmail.com>
2022-03-23 01:22:21 +0200

    added handling for blocked bishops and rooks

--- src/opening.rs
+++ src/opening.rs
@@ -2,6 +2,34 @@ use chess::{self, Board};
 use rand::{self, Rng};
 use std::fs;
 
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub struct Entry{
+    pub key: u64,
+    pub chess_move: u16,
+    pub wieght: u16,
+    pub learn: u32 
+}
+
+impl Entry{
+}
+
+
+pub fn load_opening_dataset(file_name: String){
+    let opening_table = chess::CacheTable::new(65536, Entry{key: 0, chess_move:0, wieght: 0, learn: 0});
+    let file = std::fs::read(file_name).ok().expect("error_loading data");
+    
+    
+    for byte_group in file{
+
+
+
+    }
+
+
+}
+
+
+
 pub fn get_opening_move(path: String, board: &Board) -> Option<chess::ChessMove>{
 
     let contents = fs::read_to_string(path)
